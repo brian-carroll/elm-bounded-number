fbe348ca1368c3d6854010e38907fef8168d6dfe
Brian Carroll <brian.carroll.ireland@gmail.com>
2018-03-13 01:09:20 +0000

    More precise type signatures to avoid a type-checking issue
    + and - only operate on numbers.
    Using any other comparable type produced
    weird JS type coercions in the compiled code.
    Was not caught by the Elm compiler.
    
    https://ellie-app.com/dNmT8ZQCba1/0

--- src/Number/Bounded.elm
+++ src/Number/Bounded.elm
@@ -10,17 +10,13 @@ module Number.Bounded
         , value
         )
 
-{-| A type representing bounded numbers.  Once a bound is set, the bounded value can never go out of this range.
-
-(Note that although this is intended for numbers, it works for any comparable.)
-
+{-| A type representing bounded numbers. Once a bound is set, the bounded value can never go out of this range.
 
     between 1 10
         |> set 7
         |> inc 5
         |> value
     -- (equals 10)
-
 
 @docs Bounded
 
@@ -32,11 +28,41 @@ module Number.Bounded
 
 
 {-| -}
-type Bounded comparable
-    = Bounded { min : comparable, max : comparable, value : comparable }
+type Bounded a
+    = Bounded { min : a, max : a, value : a }
 
 
-{-| Initialize a bounded number by giving it a min and max for the bounds (inclusive).  The value will be initialized as the provided min.  The min will always be the lower number, regardless of which order you provide the arguments.
+{-| Check if a number is negative, only using `number` types
+We can't use `<` since it takes `comparable`
+And `/` takes `Float`, not `Int`
+However `abs`, `*` and `^` all operate on `number`
+-}
+isNegative : number -> Bool
+isNegative x =
+    x * ((abs x) ^ -1) == -1
+
+
+{-| Minimum function defined over 'number' types rather than 'comparable' types
+-}
+minimum : number -> number -> number
+minimum a b =
+    if isNegative (a - b) then
+        a
+    else
+        b
+
+
+{-| Maximum function defined over 'number' types rather than 'comparable' types
+-}
+maximum : number -> number -> number
+maximum a b =
+    if isNegative (a - b) then
+        b
+    else
+        a
+
+
+{-| Initialize a bounded number by giving it a min and max for the bounds (inclusive). The value will be initialized as the provided min. The min will always be the lower number, regardless of which order you provide the arguments.
 -}
 between : comparable -> comparable -> Bounded comparable
 between a b =
@@ -46,7 +72,7 @@ between a b =
         Bounded { min = b, max = a, value = b }
 
 
-{-| Set the value manually.  If you try to set a value greater than the max bound, it will "clip" at the max.  Likewise, if you try to set a value less than the min bound, it will clip at the min.
+{-| Set the value manually. If you try to set a value greater than the max bound, it will "clip" at the max. Likewise, if you try to set a value less than the min bound, it will clip at the min.
 -}
 set : comparable -> Bounded comparable -> Bounded comparable
 set value (Bounded { min, max }) =
@@ -55,34 +81,34 @@ set value (Bounded { min, max }) =
 
 {-| Increments the value by the given amount, "clipping" at the max bound if it passes it.
 -}
-inc : comparable -> Bounded comparable -> Bounded comparable
+inc : number -> Bounded number -> Bounded number
 inc by (Bounded { min, max, value }) =
-    Bounded { min = min, max = max, value = Basics.min max <| value + by }
+    Bounded { min = min, max = max, value = minimum max <| value + by }
 
 
 {-| Decrements the value by the given amount, "clipping" at the min bound if it passes it.
 -}
-dec : comparable -> Bounded comparable -> Bounded comparable
+dec : number -> Bounded number -> Bounded number
 dec by (Bounded { min, max, value }) =
-    Bounded { min = min, max = max, value = Basics.max min <| value - by }
+    Bounded { min = min, max = max, value = maximum min <| value - by }
 
 
 {-| Get the value
 -}
-value : Bounded comparable -> comparable
+value : Bounded a -> a
 value (Bounded { value }) =
     value
 
 
 {-| Get the min bound
 -}
-minBound : Bounded comparable -> comparable
+minBound : Bounded a -> a
 minBound (Bounded { min }) =
     min
 
 
 {-| Get the max bound
 -}
-maxBound : Bounded comparable -> comparable
+maxBound : Bounded a -> a
 maxBound (Bounded { max }) =
     max
